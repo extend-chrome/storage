a66d29d8ff38aac8e460014aa5e1719009df2bdd
Jack Steam <teamsteamdev@gmail.com>
2019-09-06 11:32:23 -0500

    Refactor types

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,56 @@
 import { storage as rxStorage } from '@bumble/chrome-rxjs'
 import chromep from 'chrome-promise'
+import { chromepApi } from 'chrome-promise/chrome-promise'
 import { Observable } from 'rxjs'
 import { mergeMap } from 'rxjs/operators'
-import { chromepApi } from './chrome-promise'
 import {
   invalidGetter,
   invalidSetter,
   invalidSetterReturn,
 } from './validate'
 
-type StorageValues = {
-  [prop: string]: any
+/* -------------------------------------------- */
+/*                     TYPES                    */
+/* -------------------------------------------- */
+
+interface StorageValues {
+  [key: string]: any
 }
 
-type GetterFn = (values: StorageValues) => any
+interface GetterFn<T> {
+  (values: T): any
+}
 
-type Getter = string | StorageValues | GetterFn
+type NativeGetter<T> = string | string[] | T
+type Getter<T> = NativeGetter<T> | GetterFn<T>
 
-type SetterFn = (prev: StorageValues) => StorageValues
-type AsyncSetterFn = (
-  prev: StorageValues,
-) => Promise<StorageValues>
+interface SetterFn<T> {
+  (prev: T): T
+}
+interface AsyncSetterFn<T> {
+  (prev: T): Promise<T>
+}
 
-type Setter = StorageValues | SetterFn
+type Setter<T> = T | SetterFn<T>
 
-interface StorageArea {
+type Changes<T> = {
+  [K in keyof T]?: {
+    oldValue: T[K]
+    newValue: T[K]
+  }
+}
+
+type Partial<T> = {
+  [K in keyof T]?: T[K]
+}
+
+interface StorageArea<T extends StorageValues> {
   /**
    * Get a value or values in the storage area using a key name, a key name array, or a getter function.
    *
    * A getter function receives a StorageValues object and can return anything.
    */
-  get: (getter?: Getter) => Promise<any>
+  get: (getter?: Getter<T>) => Promise<T>
   /**
    * Set a value or values in the storage area using an object with keys and default values, or a setter function.
    *
@@ -38,7 +58,7 @@ interface StorageArea {
    *
    * Synchronous calls to set will be composed into a single setter function for performance and reliability.
    */
-  set: (setter: Setter) => Promise<StorageValues>
+  set: (setter: Setter<T>) => Promise<T>
   /**
    * Set a value or values in the storage area using an async setter function.
    *
@@ -54,20 +74,20 @@ interface StorageArea {
    * })
    * ```
    */
-  update: (
-    asyncSetterFn: AsyncSetterFn,
-  ) => Promise<StorageValues>
+  update: (asyncSetterFn: AsyncSetterFn<T>) => Promise<T>
   /** Remove a key from the storage area */
-  remove: (query: string) => Promise<StorageValues>
+  remove: (query: string) => Promise<void>
   /** Clear the storage area */
   clear: () => Promise<void>
   /** Emits an object with changed storage keys and StorageChange values  */
-  readonly changeStream: Observable<{
-    [key: string]: chrome.storage.StorageChange
-  }>
+  readonly changeStream: Observable<Changes<T>>
   /** Emits the current storage values when changeStream emits */
-  readonly valueStream: Observable<StorageValues>
+  readonly valueStream: Observable<T>
 }
+
+/* -------------------------------------------- */
+/*                     CODE                     */
+/* -------------------------------------------- */
 
 export const storage = {
   local: setupStorage('local'),
@@ -75,7 +95,12 @@ export const storage = {
   managed: setupStorage('managed'),
 }
 
-function setupStorage(area: string): StorageArea {
+function setupStorage<T extends StorageValues>(
+  area: string,
+): StorageArea<Partial<T>> {
+  type S = Partial<T>
+  /* ------------- GET STORAGE AREA ------------- */
+
   let storage: chromepApi.storage.StorageArea
   switch (area) {
     case 'local':
@@ -94,18 +119,22 @@ function setupStorage(area: string): StorageArea {
 
   /* --------- storage operation promise -------- */
 
-  let promise: Promise<any> | null = null
+  let promise: Promise<S> | null = null
 
-  /* ---------------- storage.get --------------- */
-  const coreGet = async (x?: any): Promise<any> => {
+  /* -------------------------------------------- */
+  /*                  STORAGE.GET                 */
+  /* -------------------------------------------- */
+
+  const coreGet = async (x?: NativeGetter<S>): Promise<S> => {
+    // Flush pending set ops before
     if (promise) {
-      await promise
+      return promise
     }
 
-    return storage.get(x || null)
+    return storage.get(x || null) as Promise<S>
   }
 
-  const get = (getter?: Getter): Promise<any> => {
+  const get = (getter?: Getter<S>): Promise<S> => {
     const errorMessage = invalidGetter(getter)
 
     if (errorMessage) {
@@ -113,18 +142,19 @@ function setupStorage(area: string): StorageArea {
     }
 
     if (getter === null || getter === undefined) {
-      return coreGet()
+      return coreGet() as Promise<S>
     } else if (Array.isArray(getter)) {
-      return coreGet(getter)
+      return coreGet(getter) as Promise<S>
     }
 
     switch (typeof getter) {
       case 'function':
-        // @ts-ignore
-        return coreGet().then(getter)
+        return (coreGet() as Promise<S>).then(getter as GetterFn<
+          S
+        >)
       case 'object':
       case 'string':
-        return coreGet(getter)
+        return coreGet(getter) as Promise<S>
       default:
         throw new TypeError(
           'Unexpected argument type: ' + typeof getter,
@@ -132,23 +162,27 @@ function setupStorage(area: string): StorageArea {
     }
   }
 
-  /* ---------------- storage.set --------------- */
-  let createNextValue = (x: any) => x
+  /* -------------------------------------------- */
+  /*                  STORAGE.SET                 */
+  /* -------------------------------------------- */
 
-  // TODO: handle async setter functions
-  const set = (arg: Setter): Promise<StorageValues> => {
+  let createNextValue = (x: S): S => x
+
+  const set = (arg: Setter<S>): Promise<S> => {
     const errorMessage = invalidSetter(arg)
 
     if (errorMessage) {
       throw new TypeError(errorMessage)
     }
 
+    // TODO: if !promise && setter !== fn, just set native
+
     return new Promise((resolve, reject) => {
-      let setter: SetterFn
+      let setter: SetterFn<S>
 
       if (typeof arg === 'function') {
         setter = (prev) => {
-          const result = arg(prev)
+          const result = (arg as SetterFn<S>)(prev)
           const errorMessage = invalidSetterReturn(result)
 
           if (errorMessage) {
@@ -196,19 +230,17 @@ function setupStorage(area: string): StorageArea {
     })
   }
 
-  // TODO: add tests for update
-  const update = async (updater: AsyncSetterFn) => {
-    const store = await get()
-    const result = await updater(store)
-    return set(result)
-  }
-
   return {
     set,
     get,
-    update,
 
-    remove(arg: string) {
+    async update(updater) {
+      const store = await get()
+      const result = await updater(store)
+      return set(result)
+    },
+
+    remove(arg) {
       const query = ([] as string[]).concat(arg)
 
       query.forEach((x) => {
@@ -219,25 +251,28 @@ function setupStorage(area: string): StorageArea {
         }
       })
 
-      return storage.remove(query).then(coreGet)
+      return storage.remove(query)
     },
 
     clear() {
-      return storage.clear().then(coreGet)
+      return storage.clear()
     },
 
     get changeStream() {
+      let stream: Observable<any>
       if (area === 'local') {
-        return rxStorage.local.changeStream
+        stream = rxStorage.local.changeStream
       } else if (area === 'sync') {
-        return rxStorage.sync.changeStream
+        stream = rxStorage.sync.changeStream
       } else {
-        return rxStorage.managed.changeStream
+        stream = rxStorage.managed.changeStream
       }
+
+      return stream as Observable<Changes<S>>
     },
 
     get valueStream() {
-      let stream: Observable<StorageValues>
+      let stream: Observable<any>
       if (area === 'local') {
         stream = rxStorage.local.changeStream
       } else if (area === 'sync') {
