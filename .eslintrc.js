d19e50093070f6452de178918d6f28bbcb355db8
Jack Steam <teamsteamdev@gmail.com>
2019-11-12 19:54:49 -0600

    Upgrade Typescript 3.7.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,22 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: 'eslint:recommended',
   globals: {
     chrome: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-console': 'off',
     quotes: 'off',
